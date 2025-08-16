7abf814d7e55b94e7639fbef623e3047f0cb048b
Wesley Henrique <wesleyhfo123@gmail.com>
2025-08-16 07:53:18 -0300

    alteração

--- 0 - var.py
+++ 0 - var.py
@@ -1,1 +1,19 @@
-import BASE
+import pandas as pd
+import BASE as bs
+import glob
+import os
+"""
+ler todos os arquivos da pasta e agrupar em um df
+"""
+
+arquivos= glob.glob(os.path.join(bs.path.p_1782_pasta, '*.xls'))
+total = len(arquivos)
+
+df = pd.DataFrame(columns= bs.col.c_1782_sug)
+
+for ir ,arquivo in enumerate(arquivos,1):
+    df_a = pd.read_excel(arquivo, header= None)
+    df = pd.concat([df, df_a], axis= 0, ignore_index= True)
+    x = df.groupby(by= 'DEP').agg(
+        qtde_itens = ('DESCRIÇÃO', 'count')
+    ).reset_index()
