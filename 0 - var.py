e78cbb27077263a2cdb270d189fd7fe07cf7457a
Wesley Henrique <wesleyhfo123@gmail.com>
2025-08-16 14:41:40 -0300

    add intrução

--- 0 - var.py
+++ 0 - var.py
@@ -14,6 +14,7 @@ df = pd.DataFrame(columns= bs.col.c_1782_sug)
 for ir ,arquivo in enumerate(arquivos,1):
     df_a = pd.read_excel(arquivo, header= None)
     df = pd.concat([df, df_a], axis= 0, ignore_index= True)
+    
     x = df.groupby(by= 'DEP').agg(
         qtde_itens = ('DESCRIÇÃO', 'count')
     ).reset_index()
