c24419deb36b1044d81a7e1b052f00b37bff95e6
Wesley Henrique <wesleyhfo123@gmail.com>
2025-10-22 17:21:35 -0300

    melhoria: conseguir fazer o consolidados e a junção das o.s agora estou fazendo as validação via excel para ver se não tem incinsistencia

--- BI_ABST.py
+++ BI_ABST.py
@@ -1,9 +1,9 @@
-from OUTROS.path_arquivos import pasta, bi_abst
+from OUTROS.path_arquivos import pasta, bi_abst, ar_xlsx
 import pandas as pd
 import numpy as pn
 import glob
 import os
-
+a = 0
 def validar_erro(e):
     print("=" * 60)
     if isinstance(e, KeyError):
@@ -16,56 +16,61 @@ def validar_erro(e):
         return f"ValueError: Erro de valor. Mensagem original: {e}"
     else:
         return f"Ocorreu um erro inesperado: {e}"
-def directory(files, argumento, ordem_colunas):
-    directory = glob.glob(os.path.join(files, argumento))
-    lista = []
-    
-    if not directory:
-        print("AVISO: Nenhum arquivo encontrado.")
-        return pd.DataFrame()
-
-    for arquivo in directory:
-        try:
-            x = pd.read_excel(arquivo)
-            print("-"*60)
-            print(x.columns)
-            print("-"*60)
-
-            X = x[ordem_colunas]
-            lista.append(x)
-        except Exception as e:
-            print(f"ERRO: Falha ao ler {os.path.basename(arquivo)}. Detalhe: {e}")
-            continue
-
-    if not lista:
-        print("AVISO: Nenhum DataFrame válido para concatenação.")
-        return pd.DataFrame()
-    
-    df_temp = pd.concat(lista, axis=0, ignore_index=True)
-    return df_temp
-
-
 
 def app():
     try:
-        ordem = [['CODFILIAL', 'NUMOS', 'DATA', 'HORA', 'CODPROD', 'DESCRICAO','ENDERECO_ORIG', 'RUA', 'PREDIO', 'NIVEL', 'APTO', 'ENDERECO_DEST','RUA_1', 'PREDIO_1', 'NIVEL_1', 'APTO_1', 'QT', 'NUMTRANSWMS','CODROTINA', 'POSICAO', 'CODFUNCGER', 'FUNCGER', 'DTFIMOS', 'CODFUNCOS','FUNCOSFIM', 'DTESTORNO', 'CODFUNCESTORNO', 'FUNCESTORNO', 'Tipo O.S.','TIPOABAST']]
-        cons28 = directory(pasta.p_28,'8628*.xlsx', ordem)
-
-        ordem = [['TIPOOS', 'NUMBONUS', 'TIPOBONUS', 'TIPOOPER', 'STATUS', 'CODPROD','DESCRICAO', 'CODFORNEC', 'FORNECEDOR', 'DTLANC', 'DATABONUS','DTFECHAMENTO', 'CODFUNCRM', 'CONFERENTE', 'CODIGOUMA', 'NUMOS','DTINICIOOS', 'CODENDERECO', 'DTFIMOS', 'CODOPERADOR', 'OPERADOR','CODEMPILHADOR', 'EMPILHADOR', 'DATAGERACAO', 'DTVALIDADE']]
-        cons64 = directory(pasta.p_64, '*8664.xlsx', ordem)
-
+        print("EXTRAÇÃO")
+        cons28 = pd.read_excel(bi_abst.cons_28)
+        cons64 = pd.read_excel(bi_abst.cons_64)
+        funcionario = pd.read_excel(ar_xlsx.ar_func, sheet_name='FUNC')
     except Exception as e:
         erros = validar_erro(e)
         print(f"EXTRAÇÃO: {erros}")
 
     try:
-        pass
+        print("TRATAMENTO")
+        cons28['DATA'] = pd.to_datetime(cons28['DATA']).dt.normalize()
+        cons28['MES'] = cons28['DATA'].dt.month
+        cons28 = cons28.loc[(cons28['MES'] != 10) & ((cons28['CODROTINA'] == 1709) | (cons28['CODROTINA'] == 1723))]
+
+        grop28 = cons28.groupby(['DATA','CODFUNCOS','FUNCOSFIM']).agg(
+            QTDE_OS = ("NUMOS", 'nunique'),
+            OS_50 = ("Tipo O.S.", lambda x: (x == '50 - Movimentação De Para').sum()),
+            OS_61 = ("Tipo O.S.", lambda x: (x == '61 - Movimentação De Para Horizontal').sum()),
+            OS_58 = ("Tipo O.S.", lambda x: (x == '58 - Transferencia de Para Vertical').sum())
+        ).reset_index()
+
+        cons64['DATAGERACAO'] = pd.to_datetime(cons64['DATAGERACAO']).dt.normalize()
+        cons64 = cons64.drop_duplicates(subset=['NUMOS'])
+        cons64['MES'] = cons64['DATAGERACAO'].dt.month
+        cons64 = cons64.loc[cons64['MES'] != 10]
+
+        grop64 = cons64.groupby(['DATAGERACAO','CODEMPILHADOR']).agg(
+            OS_RECEB = ("NUMOS", 'nunique')
+        ).reset_index()
+
+
+
+
+
+
+
+
+
+
+
+        df = funcionario['ID_NOME', 'NOME', 'AREA', 'NAME', 'SETOR'].copy()
+
+        
+        
     except Exception as e:
         erros = validar_erro(e)
         print(f"TRATAMENTO: {erros}")
 
     try:
-        cons28.to_excel(bi_abst.cons_28, index= False, sheet_name= "8628")
+        print("CARGA")
+
+        # cons28.to_excel(bi_abst.cons_28, index= False, sheet_name= "8628")
         cons64.to_excel(bi_abst.cons_64, index= False, sheet_name= "8664")
     except Exception as e:
         erros = validar_erro(e)
