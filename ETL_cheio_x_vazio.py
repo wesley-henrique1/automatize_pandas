d2c4e50a7f9d12320a518ed550d379404e1b2c48
Wesley Henrique <wesleyhfo123@gmail.com>
2025-11-21 09:56:48 -0300

    "style: Adicionar informativos dos dados Adiciona prints pelo decorrer do script para facilitar a identificação na hora de gerar os relatórios"

--- ETL_cheio_x_vazio.py
+++ ETL_cheio_x_vazio.py
@@ -27,27 +27,21 @@ def app():
 
     try:
         for arquivo in arquivos:
-            names_files = os.path.basename(arquivo)
-
             try:
+                names_files = os.path.basename(arquivo)
                 x = pd.read_excel(arquivo, header= 2)
+                x['NAMES_FILES'] = names_files
                 lista.append(x)
             except Exception as e:
-                error = validar_erro(e)
-                print("_" * 36)
-                print(f"leituras: {e}")
                 lista_erros.append(names_files)
-                print(f"erro: {names_files}\n")
-                print("_" * 36)
                 continue
         
         if lista:
             df_temp = pd.concat(lista, axis= 0, ignore_index= True)
-
             df = df_temp.loc[df_temp['Retorno'] != 'SEM RETORNO'].copy()
-
             df = df.drop(columns= ['Unnamed: 11', 'Unnamed: 12','Unnamed: 13', 'Unnamed: 14'])
             df = df.dropna(subset=['END'])
+            total_lista = len(lista)
             print("\nProcessamento de DataFrames concluído com sucesso.")
         else:
             print("\nNenhuma planilha válida foi encontrada para processamento.")
@@ -57,6 +51,12 @@ def app():
 
     try:
         df.to_excel(output.cheio_vazio, index=False, sheet_name= "FATO_GERAL")
+
+        total_erros = len(lista_erros)
+        total_lista = len(lista)
+        
+        print(f"{total_lista} arquivos processados.\n")
+        print(f"{total_erros} arquivos com divergencias.\n")
     except Exception as e:
         error = validar_erro(e)
         print(f"Carga: {error}\n")
