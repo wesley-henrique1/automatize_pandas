665f6a990625d5bebc466ab33ffb36d59bc64725
Wesley Henrique <wesleyhfo123@gmail.com>
2025-12-26 08:23:46 -0300

    Refactor: Consolida scripts de produtos e fixa conflito de sys.path
    - Mescla scripts de produtos parados e FL no novo BI_GIRO_STATUS.py.
    - Corrige conflito onde o Python ignorava o ambiente virtual (.meu_ambiente).
    - Implementa inserção manual via sys.path para garantir o carregamento correto dos módulos (Pandas, SQLAlchemy).

--- ETL_CORTE.py
+++ ETL_CORTE.py
@@ -1,107 +1,145 @@
+import sys
+caminho_env = r'C:\Users\wesley.oliveira\WS_OLIVEIRA\SCRIPTS\.meu_ambiente\Lib\site-packages'
+if caminho_env not in sys.path:
+    sys.path.insert(0, caminho_env)
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import URL
+
 from config.config_path import *
+from config.fuction import Funcao
 import pandas as pd
 import warnings
 import glob
 import os 
 warnings.simplefilter(action='ignore', category=UserWarning)
 
-def validar_erro(e):
-    print("=" * 60)
-    if isinstance(e, KeyError):
-        return f"KeyError: A coluna ou chave '{e}' não foi encontrada."
-    elif isinstance(e, PermissionError):
-        return "PermissionError: O arquivo está sendo usado ou você não tem permissão para acessá-lo. Por favor, feche o arquivo."
-    elif isinstance(e, TypeError):
-        return f"TypeError: Erro de tipo. Verifique se os dados são do tipo correto. Mensagem original: {e}"
-    elif isinstance(e, ValueError):
-        return f"ValueError: Erro de valor. Mensagem original: {e}"
-    else:
-        return f"Ocorreu um erro inesperado: {e}"  
-def ajuste_col(df_original, col):
-            df_copia = df_original.copy() 
-            for i in col:
-                df_copia[i] = df_copia[i].fillna(0).astype(str)
-                df_copia[i] = df_copia[i].str.replace(".", "", regex=False)
-                df_copia[i] = df_copia[i].str.replace(",", ".")
-                df_copia[i] = df_copia[i].fillna(0).astype(float)
-            return df_copia
-def correcao(df, dt):
-    var = df.copy()
 
-    var['dia'] = var[dt].dt.day_name('pt_BR')
-    var['mes'] = var[dt].dt.month_name('pt_BR')
-    var['ano'] = var[dt].dt.year
-    var[dt] = var[dt].dt.strftime("%d-%m-%Y")     
-    var['vl_corte'] = var['vl_corte'].round(2).astype(str).str.replace('.', ',', regex= False)
-    var = var.sort_values(by=dt, ascending= True, axis= 0)
-    return var
-def loop_apre(df1, id):
-    if id == 1:
-        print(f"\n{"Relatorio corte":-^88}\n")
-        print(f"{"DIA":-^88}\n",
-            f"{"DATA":<12} {"VL_CORTE":<12} {"QTDE_CORTE":<10} {"QTDE_ITEM":<12} {"DIA":<14} {"MÊS":<14} {"ANO":<4}")
-        for x in df1.itertuples():
-            print(f"|{x.data:<12} {x.vl_corte:<12} {x.qtde_corte:<10} {x.qtde_item:<12} {x.dia:<14} {x.mes:<14} {x.ano:<4}|")
-        print("-" * 88)
-        print("\n")
-
-    elif id == 2:
-        print(f"{"NOITE":-^88}\n",
-            f"{"DATA":<12} {"VL_CORTE":<12} {"QTDE_CORTE":<10} {"QTDE_ITEM":<12} {"DIA":<14} {"MÊS":<14} {"ANO":<4}")
-        for x in df1.itertuples():
-            print(f"|{x.data_turno:<12} {x.vl_corte:<12} {x.qtde_corte:<10} {x.qtde_item:<12} {x.dia:<14} {x.mes:<14} {x.ano:<4}|")
-        print("-" * 88)
-        print("\n")
+class corte:
+    def __init__(self):
+        DRIVER = acumulados_db.drive
+        DB_ACUMULADO = acumulados_db.db_acumulado
+        self.NOME_TABELA = acumulados_db.db_8041
+        self.ODBC_CONN_STR = (f"DRIVER={DRIVER};" f"DBQ={DB_ACUMULADO};")
 
 
-    elif id == 3:
-        print(f"{"DIVERGENCIA":-^88}\n",
-            f"{"DATA":<14} {"QTDE_PEDIDOS":<14} {"PED_IMPERFEITO":<14} {"DIA":<14} {"MÊS":<14} {"ANO":<4}")
+        self.pipeline()
+        input()
+
+    def conectar_db(self, coluna,tabela):
+        query_select = f"SELECT {coluna} FROM {tabela}"
+        query_tb = f"SELECT * FROM {tabela}"
+
+        try:
+            connection_url = URL.create(
+            "access+pyodbc",
+            query={"odbc_connect":  self.ODBC_CONN_STR}
+            )
+            self.engine = create_engine(connection_url)
+            with self.engine.connect() as connection:
+                result = connection.execute(text(query_select))
+                dados_existentes = result.fetchall()
+
+                result_1 = connection.execute(text(query_tb))
+                tb_dados = result_1.fetchall()
+                nomes_colunas = result_1.keys()
+                df_tb_dados = pd.DataFrame(
+                    tb_dados,
+                    columns=nomes_colunas
+                )
+
+                print("Conectado com sucesso ao Access!")
+                print("DataFrame criado com sucesso!")
+        except Exception as e:
+            error = Funcao.validar_erro(e)
+            print(error)
+            
+        return dados_existentes, df_tb_dados        
+    def atualizar(self, df, tabela):
+        try:            
+            df.to_sql(
+                name= tabela,
+                con= self.engine,
+                if_exists='append',
+                index=False,
+            )
+            print(f"DataFrame salvo com sucesso na tabela '{tabela}' no Access.")
+        except Exception as e:
+            error = Funcao.validar_erro(e)
+            print(error)
+            exit()
+    def loop_apre(self, df1, id):
+        if   id == 1:
+            print(f"\n{"Relatorio corte":-^88}\n")
+            print(f"{"DIA":-^88}\n",
+                f"{"DATA":<12} {"VL_CORTE":<12} {"QTDE_CORTE":<10} {"QTDE_ITEM":<12} {"DIA":<14} {"MÊS":<14} {"ANO":<4}")
+            for x in df1.itertuples():
+                print(f"|{x.data:<12} {x.vl_corte:<12} {x.qtde_corte:<10} {x.qtde_item:<12} {x.dia:<14} {x.mes:<14} {x.ano:<4}|")
+            print("-" * 88)
+            print("\n")
+        elif id == 2:
+            print(f"{"NOITE":-^88}\n",
+                f"{"DATA":<12} {"VL_CORTE":<12} {"QTDE_CORTE":<10} {"QTDE_ITEM":<12} {"DIA":<14} {"MÊS":<14} {"ANO":<4}")
+            for x in df1.itertuples():
+                print(f"|{x.data_turno:<12} {x.vl_corte:<12} {x.qtde_corte:<10} {x.qtde_item:<12} {x.dia:<14} {x.mes:<14} {x.ano:<4}|")
+            print("-" * 88)
+            print("\n")
+        elif id == 3:
+            print(f"{"DIVERGENCIA":-^88}\n",
+                f"{"DATA":<14} {"QTDE_PEDIDOS":<14} {"PED_IMPERFEITO":<14} {"DIA":<14} {"MÊS":<14} {"ANO":<4}")
+            
+            for x in df1.itertuples():
+                data_formatada = x.data_turno.strftime('%d-%m-%Y')
+                print(f"|{data_formatada:<14} {x.QTDE_PED:<14} {x.ped_imperfeito:<14} {x.dia:<14} {x.mes:<14} {x.ano:<4}|")
+            print("-" * 88)
+            print("\n")            
+
+
+    def pipeline(self):
+        try: # Extração dos dados nessecario 
+            files = glob.glob(os.path.join(directory.dir_41, '*.xls*'))
+            names_db, base_cons = self.conectar_db('NOME_ARQUIVO', self.NOME_TABELA)
+            arquivos_processados = set([tupla[0].strip() for tupla in names_db if tupla[0] is not None])
+
+            df_corte = pd.read_csv(wms.wms_67,header= None, names= col_names.col_67)
+            print(len(arquivos_processados))
+        except Exception as e:
+            error = Funcao.validar_erro(e)
+            print(F"Extração: {error}")
         
-        for x in df1.itertuples():
-            data_formatada = x.data_turno.strftime('%d-%m-%Y')
-            print(f"|{data_formatada:<14} {x.Qtde_pedido:<14} {x.ped_imperfeito:<14} {x.dia:<14} {x.mes:<14} {x.ano:<4}|")
-        print("-" * 88)
-        print("\n")
-
-
-def app():
-    try: # Extração dos dados nessecario 
-        files = glob.glob(os.path.join(directory.dir_41, '*.xls*'))
-        df_corte = pd.read_csv(wms.wms_67,header= None, names= col_names.col_67)
-        base_cons = pd.read_excel(output.acum_41, sheet_name= 'acumulado_ped')
-    except Exception as e:
-        error = validar_erro(e)
-        print(F"Extração: {error}")
-    
-    try:
+        try:
             try:
                 df_corte['hora'] = df_corte['hr'].astype(str) + ':' + df_corte['min'].astype(str)
                 df_corte['data'] = pd.to_datetime(df_corte['data'], format= '%d/%m/%y').sort_index(axis= 0, ascending= True)
                 df_corte['hora'] = pd.to_datetime(df_corte['hora'], format= '%H:%M').dt.strftime('%H:%M')
 
                 col_ajustar = ['vl_corte', 'qtde_corte']
-                df_corte = ajuste_col(df_corte, col_ajustar)
+                for col in col_ajustar:
+                    df_corte = Funcao.ajustar_numero(df_corte, col, float)
                 df_corte = df_corte.sort_values(by="data", ascending= True, axis= 0)
             except Exception as e:
-                error = validar_erro(e)
+                error = Funcao.validar_erro(e)
                 print(F"\nCortes geral: {error}")
 
             try:
                 df_dia = df_corte.loc[df_corte['hora'].between("07:30:00", "18:00:00")].copy()
+
                 var_dia = df_dia.groupby('data').agg(
                             vl_corte=('vl_corte', 'sum'),
                             qtde_corte=('qtde_corte', 'count'),
                             qtde_item=('desc', 'nunique')
                         ).reset_index()
-                var_dia = correcao(var_dia, 'data')
+                
+                var_dia = Funcao.extrair_e_ordenar_data(var_dia, 'data')
+                var_dia['vl_corte'] = var_dia['vl_corte'].round(2).astype(str).str.replace('.', ',', regex= False)
                 var_dia = var_dia.sort_values(by= 'data', ascending= True, axis= 0)
             except Exception as e:
-                error = validar_erro(e)
+                error = Funcao.validar_erro(e)
                 print(F"\nCortes dias: {error}")
 
             try:
                 df_noite = df_corte.loc[~df_corte['hora'].between("07:30:00", "18:00:00")].copy()
+
                 df_noite['data'] = pd.to_datetime(df_noite['data'], format= '%d/%m/%Y')
                 df_noite["data_turno"] = df_noite['data'].copy()
                 df_noite.loc[df_noite['hora'] < "07:30:00", 'data_turno'] -= pd.Timedelta(days=1)
@@ -110,61 +148,67 @@ def app():
                     qtde_corte=('qtde_corte', 'count'),
                     qtde_item=('desc', 'nunique')
                 ).reset_index()
-                var_noite = correcao(var_noite, 'data_turno')
+                var_noite = Funcao.extrair_e_ordenar_data(var_noite, 'data_turno')
+                var_noite['vl_corte'] = var_noite['vl_corte'].round(2).astype(str).str.replace('.', ',', regex= False)
                 var_noite = var_noite.sort_values(by='data_turno', ascending= True, axis= 0)
             except Exception as e:
-                error = validar_erro(e)
+                error = Funcao.validar_erro(e)
                 print(F"\nCorte noite: {error}")
 
             try:
-                total_files = len(files)
-                lista_name = []
-                lista_files = []
-                if total_files == 0:
+                list_consolidado = []
+                list_processados = []
+                dic_erros = {}
+
+                if not files:
                     print("Nenhum arquivo Excel encontrado na pasta especificada.")
-                    return None
                 
                 for file in files:
                     name_file = os.path.basename(file)
-                    valor_existe = (base_cons['Nome_arquivo'] == name_file).any()
-                    if valor_existe:
+                    if name_file in arquivos_processados:
+                        list_consolidado.append(name_file)
                         continue
-                    lista_name.append(name_file)
-                    try:
+
+                    try: # PROCESSAR ARQUIVOS DA PASTA
                         df_var = pd.read_excel(file)
                         if 'NUMPED' not in df_var.columns:
-                            print(f"AVISO: Coluna 'NUMPED' não encontrada no arquivo {name_file}\n")
+                            dic_erros['PED_COLUMNS'] = str(name_file), "COLUNA 'NUMPED' INEXISTENTE"
                             continue
+
                         pedido = df_var['NUMPED'].nunique()
                         produto = df_var['PRODUTO'].nunique()
 
                         df_pedidos = pd.DataFrame({
-                            "Nome_arquivo" :  [name_file]
-                            ,"Qtde_pedido"  : [pedido]
-                            ,"Qtde_produto" : [produto]
+                            "NOME_ARQUIVO" :  [name_file]
+                            ,"QTDE_PED"  : [pedido]
+                            ,"QTDE_PROD" : [produto]
                         })
-                        lista_files.append(df_pedidos)
-                        contagem_name = len(lista_name)
-                        print(f"Contagem de arquivos:{contagem_name}\n")
+                        list_processados.append(df_pedidos)
+                        
                     except Exception as e:
-                        error = validar_erro(e)
-                        print(F"Extração: {error}")
+                        error = Funcao.validar_erro(e)
+                        dic_erros['PED_PASTA'] = str(name_file), str(error)
                         continue
-                if not lista_files:
-                    print("lista vazia")
+
+                if not list_processados:
                     df_cons = base_cons.copy()
-                    df_cons['DATA'] = pd.to_datetime(df_cons['DATA'], format='%d-%m-%Y')
+                    if 'DATA' in df_cons.columns:
+                        df_cons['DATA'] = pd.to_datetime(df_cons['DATA'], format='%d-%m-%Y', errors='coerce')
                 
                 else:
-                    df_cons = pd.concat(lista_files, ignore_index= True)
+                    df_novos = pd.concat(list_processados, ignore_index= True)
+
+                    df_novos['DATA_BRUTA'] = df_novos['NOME_ARQUIVO'].str.extract(r'(\d{2}[-_]\d{2})')
+                    df_novos['DATA_COMPLETA'] = df_novos['DATA_BRUTA'].str.replace('_', '-') + '-2025'
+                    df_novos['DATA'] = pd.to_datetime(df_novos['DATA_COMPLETA'], format='%d-%m-%Y')
+
+                    df_novos.drop(columns=['DATA_BRUTA', 'DATA_COMPLETA'], inplace=True)
+                    df_novos = df_novos[['DATA','QTDE_PROD', 'QTDE_PED', 'NOME_ARQUIVO']]
+
                     df_cons = pd.concat([base_cons, df_cons], ignore_index= True).sort_values(by='DATA', ascending= True, axis= 0)
                     df_cons = df_cons.drop_duplicates(subset= None, keep= 'first')
-                    df_cons['DATA_BRUTA'] = df_cons['Nome_arquivo'].str.extract(r'(\d{2}[-_]\d{2})')
-                    df_cons['DATA_COMPLETA'] = df_cons['DATA_BRUTA'].str.replace('_', '-') + '-2025'
-                    df_cons['DATA'] = pd.to_datetime(df_cons['DATA_COMPLETA'], format='%d-%m-%Y')
-                    df_cons.drop(columns=['DATA_BRUTA', 'DATA_COMPLETA'], inplace=True)
             except Exception as e:
-                error = validar_erro(e)
+                error = Funcao.validar_erro(e)
                 print(F"\nPedidos: {error}")
             
             try:
@@ -179,49 +223,61 @@ def app():
                 var_div['mes'] = var_div['data_turno'].dt.month_name('pt_BR')
                 var_div['ano'] = var_div['data_turno'].dt.year
                 var_div['data_turno'] = pd.to_datetime(var_div['data_turno'], format='%d-%m-%Y')
-                var_div = var_div.merge(df_cons, left_on= 'data_turno', right_on= 'DATA', how= 'left').drop(columns= {'Nome_arquivo', 'Qtde_produto'}).sort_values(by="data_turno", ascending= True, axis= 0)
+                var_div = var_div.merge(df_cons, left_on= 'data_turno', right_on= 'DATA', how= 'left').drop(columns= {'NOME_ARQUIVO', 'QTDE_PROD'}).sort_values(by="data_turno", ascending= True, axis= 0)
             except Exception as e:
-                error = validar_erro(e)
+                error = Funcao.validar_erro(e)
                 print(F"\nDivergencia: {error}")
-    except Exception as e:
-        error = validar_erro(e)
-        print(F"\nTratamento: {error}")
-
-    try:
-        max_ex_dia= df_dia['data'].max() 
-        max_ex_noite = df_noite['data_turno'].max()
-
-        ex_dia = df_dia.loc[df_dia['data'] == max_ex_dia]
-        ex_noite = df_noite.loc[df_noite['data_turno'] == max_ex_noite]
-
-        with pd.ExcelWriter(output.corte) as destino_corte:
-            df_corte.to_excel(destino_corte, sheet_name='extrato', index= False)
-            ex_dia.to_excel(destino_corte, sheet_name= 'ex_dia', index= False)
-            ex_noite.to_excel(destino_corte, sheet_name= 'ex_noite', index= False)
-
-        df_cons.to_excel(output.acum_41, sheet_name= 'acumulado_ped', index= False)
-    except Exception as e:
-        error = validar_erro(e)
-        print(F"Carga: {error}")
-
-    try:
-        print("-" * 88)
-        print('\nfim do processo, verifique o arquivo controle_corte.xlsx')
-        loop_apre(var_dia, 1)
-        loop_apre(var_noite, 2)
-        loop_apre(var_div, 3)
+        except Exception as e:
+            error = Funcao.validar_erro(e)
+            print(F"\nTratamento: {error}")
 
         try:
-            print(f"{contagem_name} arquivos consolidados com sucesso.\n")
+            max_ex_dia= df_dia['data'].max() 
+            max_ex_noite = df_noite['data_turno'].max()
+
+            ex_dia = df_dia.loc[df_dia['data'] == max_ex_dia].copy()
+            ex_noite = df_noite.loc[df_noite['data_turno'] == max_ex_noite].copy()
+            
+
+            with pd.ExcelWriter(output.corte) as destino_corte:
+                df_corte.to_excel(destino_corte, sheet_name='extrato', index= False)
+                ex_dia.to_excel(destino_corte, sheet_name= 'ex_dia', index= False)
+                ex_noite.to_excel(destino_corte, sheet_name= 'ex_noite', index= False)
+
+            if list_processados:
+                self.atualizar(df_novos, self.NOME_TABELA)
         except Exception as e:
-            print("AVISO: Nenhum novo arquivo foi consolidado nesta execução.\n")
+            error = Funcao.validar_erro(e)
+            print(F"Carga: {error}")
 
-        print("-" * 88)    
-    except Exception as e:
-        error = validar_erro(e)
-        print(F"Apresentação: {error}")
+        try:
+            print("-" * 88)
+            self.loop_apre(var_dia, 1)
+            self.loop_apre(var_noite, 2)
+            self.loop_apre(var_div, 3)
 
-if __name__ == "__main__":
-    app()
-    input("\nPressione Enter para sair...")
+            if not list_consolidado:
+                print(f"{len(list_processados)} arquivos consolidados com sucesso.\n")
+            elif list_consolidado:
+                print("Nenhum novo arquivo foi consolidado nesta execução.\n")
 
+            LARGURA = 88
+
+            if dic_erros:
+                print(f"{'RELATÓRIO DE FALHAS':^{LARGURA}}")
+                print("-" * LARGURA)
+                for etapa, detalhe_erro in dic_erros.items():
+                    nome_arquivo = detalhe_erro[0]
+                    descricao_erro = detalhe_erro[1]
+                    print(f"ETAPA DA FALHA: {etapa}")
+                    print(f"  > Arquivo Afetado: {nome_arquivo}")
+                    print(f"  > Causa: {descricao_erro}")
+                    print("-" * LARGURA)
+            print("-" * 88)    
+        except Exception as e:
+            error = Funcao.validar_erro(e)
+            print(F"Apresentação: {error}")
+
+if __name__ == '__main__':
+    corte()
+    input("Aperte 'enter' para finalizar o processo...")
--- ETL_cheio_x_vazio.py
+++ ETL_cheio_x_vazio.py
@@ -1,30 +1,86 @@
-from config.config_path import *
+import sys
+caminho_env = r'C:\Users\wesley.oliveira\WS_OLIVEIRA\SCRIPTS\.meu_ambiente\Lib\site-packages'
+if caminho_env not in sys.path:
+    sys.path.insert(0, caminho_env)
+from config.config_path import directory, acumulados_db
 from config.fuction import Funcao
 import pandas as pd
 import glob
 import os
 
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import URL
+
+
 class cheio_vazio:
     def __init__(self):
-        self.list_dir = [directory.dir_cheio_vazio]
-        pass
+        DRIVER = acumulados_db.drive
+        DB_ACUMULADO = acumulados_db.db_acumulado
+        self.NOME_TABELA = acumulados_db.tb_vz_ch
 
-    def carregamento(self):
-        pass
+        self.ODBC_CONN_STR = (
+            f"DRIVER={DRIVER};"
+            f"DBQ={DB_ACUMULADO};"
+        )
 
-    def pipeline(self):         
+        self.dados = self.pipeline()
+
+
+    def conectar_db(self, tabela):
+        query_select = f"SELECT NOME_RELATORIO FROM {tabela}"
+
         try:
-            pasta_files = glob.glob(os.path.join(directory.dir_cheio_vazio, "*xls*"))
+            connection_url = URL.create(
+            "access+pyodbc",
+            query={"odbc_connect":  self.ODBC_CONN_STR}
+            )
+            self.engine = create_engine(connection_url)
+            with self.engine.connect() as connection:
+                result = connection.execute(text(query_select))
+                dados_existentes = result.fetchall()
+                print("Conectado com sucesso ao Access!")
         except Exception as e:
-            self.erro = Funcao.validar_erro(e)
-            print(f"Extração: {self.erro}\n")
+            error = Funcao.validar_erro(e)
+            print(error)
+            exit()
+            
+        return dados_existentes
+    def atualizar(self, df, tabela):
+        try:            
+            df.to_sql(
+                name= tabela,
+                con= self.engine,
+                if_exists='append',
+                index=False,
+            )
+            print(f"DataFrame salvo com sucesso na tabela '{tabela}' no Access.")
+        except Exception as e:
+            error = Funcao.validar_erro(e)
+            print(error)
+            exit()
 
-        try:
+    def pipeline(self):
+        try:  # EXTRAÇÃO DOS DADOS
+            dados_db = self.conectar_db(self.NOME_TABELA)
+
+            pasta_files = glob.glob(os.path.join(directory.dir_cheio_vazio, "*xls*"))
+
             list_processado = []
+            list_files_db = []
             list_erros = {}
+        except Exception as e:
+            error = Funcao.validar_erro(e)
+            print(error)
+            exit()
 
+        try: # TRATAMENTO DOS DADOS
+            arquivos_processados = set([tupla[0] for tupla in dados_db])
+            
             for file in pasta_files:
                 NAME_FILE = os.path.basename(file)
+                if NAME_FILE in arquivos_processados:
+                    list_files_db.append(NAME_FILE)
+                    continue
                 try:
                     df = pd.read_excel(file, header= 2)
                     df_filtrado = df.loc[df['Retorno'].isin(['CHEIO', 'VAZIO'])]
@@ -33,26 +89,60 @@ class cheio_vazio:
                     list_processado.append(df_filtrado)
                 except Exception as e:
                     list_erros['ETAPA_CONSOLIDADO'] = str(NAME_FILE), str(e)
-                
+
             df_consolidado = pd.concat(list_processado, axis= 'index')
+
+            list_renomear = {'DESCRIÇÃO':'DESC', 'data_relatorio':'DATA_RELATORIO', 'NAME_FILE':'NOME_RELATORIO', 'Retorno':'RETORNO'}
+            df_consolidado = df_consolidado.rename(columns=list_renomear)
 
             col_int = ['END', 'COD','RUA','PREDIO','NIVEL','APTO']
             for col in col_int:
                 try:
                     df_consolidado[col] = df_consolidado[col].fillna(0).astype(int)
                 except Exception as e:
-                    list_erros.clear()
-                    list_erros[col] = str(e)
+                    list_erros['LIMPEZA'] = str(col),str(e)
+
         except Exception as e:
-            self.erro = Funcao.validar_erro(e)
-            print(f"Tratamento: {self.erro}\n")
+            error = Funcao.validar_erro(e)
+            print(error)
+            exit()
 
         try:
-            pass
-        except Exception as e:
-            self.erro = Funcao.validar_erro(e)
-            print(f"Carga: {self.erro}\n")
+            LARGURA = 50 
 
-if __name__ == "__main__":
+            print("\n" + "=" * LARGURA)
+            print(f"{'RELATÓRIO DE PROCESSAMENTO':^{LARGURA}}")
+            print("=" * LARGURA)
+
+            print(f"{'ESTATÍSTICAS DE ARQUIVOS':^{LARGURA}}")
+            print("-" * LARGURA)
+            print(f"| {'Arquivos Processados:':<30}| {len(list_processado):>15}|")
+            print(f"| {'Registros Salvos no Banco:':<30}| {len(list_files_db):>15}|")
+            print("-" * LARGURA)
+
+            if list_erros:
+                print(f"{'RELATÓRIO DE FALHAS':^{LARGURA}}")
+                print("-" * LARGURA)
+                for etapa, detalhe_erro in list_erros.items():
+                    nome_arquivo = detalhe_erro[0]
+                    descricao_erro = detalhe_erro[1]
+                    print(f"ETAPA DA FALHA: {etapa}")
+                    print(f"  > Arquivo Afetado: {nome_arquivo}")
+                    print(f"  > Causa: {descricao_erro}")
+                    print("-" * LARGURA)
+            else:
+                print(f"{'STATUS: PROCESSAMENTO CONCLUÍDO SEM ERROS':^{LARGURA}}")
+
+            print(f"\n{'ATUALIZAÇÃO DE DADOS INICIADA':^{LARGURA}}")
+            print(f"Tentando salvar {len(list_files_db)} registros na tabela '{self.NOME_TABELA}'...")
+            print("-" * LARGURA + "\n")
+
+            self.atualizar(df_consolidado, self.NOME_TABELA)
+        except Exception as e:
+            error = Funcao.validar_erro(e)
+            print(error)
+
+
+if __name__ == '__main__':
     cheio_vazio()
-    input("\nPressione Enter para sair...")
+    input("Aperte 'enter' para finalizar o processo...")
