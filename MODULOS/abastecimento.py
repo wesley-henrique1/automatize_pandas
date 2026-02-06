a123629589678d35fc4faa056634214a2cc36735
Wesley Henrique <wesleyhfo123@gmail.com>
2026-02-06 16:52:18 -0300

    fix(logica): ajusta HR_AM para 06:30:00, limpa duplicatas e refatora funções

--- MODULOS/abastecimento.py
+++ MODULOS/abastecimento.py
@@ -1,4 +1,5 @@
-from modulos._settings import Power_BI, Relatorios
+# from modulos._settings import Power_BI, Relatorios
+from _settings import Power_BI, Relatorios
 import datetime as dt
 import pandas as pd
 import numpy as np
@@ -35,56 +36,19 @@ class auxiliar:
                 f.write(log_conteudo)
         except Exception as erro_f:
             print(f"Falha crítica ao gravar log: {erro_f}")
-    def organizar_df(self, df_original, col, id):
-        df = df_original
-        df[col] = pd.to_datetime(df[col]).dt.normalize()
-        df['MES'] = df[col].dt.month
-
-        if id == 1:
-            return df.loc[(df['CODROTINA'] == 1709) | (df['CODROTINA'] == 1723)]
-
-        elif id ==2: 
-            df = df[['DATAGERACAO', 'DTLANC', 'NUMBONUS', 'NUMOS', 'CODEMPILHADOR','EMPILHADOR', 'MES']].copy()
-            df = df.drop_duplicates(subset=['NUMOS'])
-            return df
-    def agrupar(self, df, col, id):
+    def agrupar(self, df, col):
         df = df.copy()
-        if id == 1:
-            df['IS_50'] = (df['Tipo O.S.'] == '50 - Movimentação De Para').astype(int)
-            df['IS_61'] = (df['Tipo O.S.'] == '61 - Movimentação De Para Horizontal').astype(int)
-            df['IS_58'] = (df['Tipo O.S.'] == '58 - Transferencia de Para Vertical').astype(int)
-            temp = df.groupby(col).agg(
-                QTDE_OS = ("NUMOS", 'nunique'),
-                OS_50 = ("IS_50", "sum"),
-                OS_61 = ("IS_61", "sum"),
-                OS_58 = ("IS_58", "sum")
-            ).reset_index()
-            return temp
-        elif id == 2:
-            temp = df.groupby(col).agg(
-                OS_RECEB = ("NUMOS", 'nunique')
-            ).reset_index()
-            return temp
-        elif id == 3:
-            temp = df.groupby(col).agg(
-                OS_RECEB = ('OS_RECEB', 'sum')
-            ).reset_index()
-            temp[col] = pd.to_datetime(temp[col]).dt.normalize()
-            return temp
-        elif id == 4:
-            temp = df.groupby(col).agg(
-                QTDE_OS = ("QTDE_OS", 'sum'),
-                OS_50 = ("OS_50", 'sum'),
-                OS_61 = ("OS_61", 'sum'),
-                OS_58 = ("OS_58", 'sum')
-            ).reset_index()
-            temp[col] = pd.to_datetime(temp[col]).dt.normalize()
-            return temp 
-        elif id == 5:
-            temp = df.groupby([col]).agg(
-                    TOTAL_BONUS = ("NUMBONUS", "nunique")
-                ).reset_index().sort_values(by=col, ascending= False)  
-            return temp 
+
+        df['IS_50'] = (df['Tipo O.S.'] == '50 - Movimentação De Para').astype(int)
+        df['IS_61'] = (df['Tipo O.S.'] == '61 - Movimentação De Para Horizontal').astype(int)
+        df['IS_58'] = (df['Tipo O.S.'] == '58 - Transferencia de Para Vertical').astype(int)
+        temp = df.groupby(col).agg(
+            QTDE_OS = ("NUMOS", 'nunique'),
+            OS_50 = ("IS_50", "sum"),
+            OS_61 = ("IS_61", "sum"),
+            OS_58 = ("IS_58", "sum")
+        ).reset_index()
+        return temp
     def corrigir_hr(self, valor):
         valor = str(valor).strip()
         
@@ -108,6 +72,9 @@ class auxiliar:
 class Abastecimento(auxiliar):
     def __init__(self):
         self.list_path = [Relatorios.rel_28, Relatorios.rel_64]
+        self.rotinas_filtro = [1723,1709]
+        self.hr_AM = "06:30:00"
+        self.hr_PM = "18:00:00"
         self.pipeline()
 
     def pipeline(self):
@@ -116,8 +83,8 @@ class Abastecimento(auxiliar):
             'DTFIMOS', 'CODFUNCOS', 'FUNCOSFIM', 'Tipo O.S.', 'TIPOABAST']
             cols_64 = ['DATAGERACAO', 'DTLANC', 'NUMBONUS', 'NUMOS', 'CODEMPILHADOR', 'EMPILHADOR']
 
-            m_atual28 = pd.read_excel(Relatorios.rel_28, usecols=cols_28, engine='openpyxl')
-            m_atual64 = pd.read_excel(Relatorios.rel_64, usecols=cols_64, engine='openpyxl')
+            m_atual28 = pd.read_excel(self.list_path[0], usecols=cols_28, engine='openpyxl')
+            m_atual64 = pd.read_excel(self.list_path[1], usecols=cols_64, engine='openpyxl')
 
             dt_maior_28 = m_atual28['DATA'].max()
             dt_menor_28 = m_atual28['DATA'].min()
@@ -128,34 +95,33 @@ class Abastecimento(auxiliar):
                 f"\n8628 >> {dt_menor_28:%d/%m/%Y} -- {dt_maior_28:%d/%m/%Y}\n"
                 f"8664 >> {dt_menor_64:%d/%m/%Y} -- {dt_maior_64:%d/%m/%Y}"
             )
-
-            print(f"debug: Extract")
+            print("PASSAGEM EXTRAÇÃO\n")
         except Exception as e:
             self.validar_erro(e, "Extract")
             return False
 
         try:
             try:
-                # CORRIGINDO BASE ORIGINAL E SEPARANDO EM OUTRAS PARTE, PARA MELHOR VISUALIZAÇÃO
-                m_atual28 = self.organizar_df(m_atual28,'DATA',1)
+                m_atual28['DATA'] = pd.to_datetime(m_atual28['DATA']).dt.normalize()
+                m_atual28['MES'] = m_atual28['DATA'].dt.month
                 m_atual28['HORA'] = m_atual28["HORA"].apply(self.corrigir_hr)
+                m_atual28 = m_atual28.loc[m_atual28['CODROTINA'].isin(self.rotinas_filtro)]
                 
                 cond_turno = (
-                    (m_atual28['HORA'] >= "07:00:00")  & (m_atual28['HORA'] <= "18:00:00")
-                    ,(m_atual28['HORA'] >= "18:00:00") | (m_atual28['HORA'] < "07:00:00")
+                    (m_atual28['HORA'] >= self.hr_AM)  & (m_atual28['HORA'] <= self.hr_PM)  # CONDIÇÃO 1
+                    ,(m_atual28['HORA'] >= self.hr_PM) | (m_atual28['HORA'] < self.hr_AM)   # CONDIÇÃO 2
                 )
                 result_turno = (
-                    m_atual28['DATA']
-                    ,m_atual28['DATA'] - pd.Timedelta(days= 1)
+                    m_atual28['DATA']                               # CONDIÇÃO 1
+                    ,m_atual28['DATA'] - pd.Timedelta(days= 1)      # CONDIÇÃO 2
                 )
                 m_atual28['DT_TURNO'] = np.select(cond_turno, result_turno, default=m_atual28['DATA'])
+                pendencia = m_atual28.loc[m_atual28['POSICAO'] == "P"]
 
 
-                os_geradas28 = self.agrupar(m_atual28, ['DT_TURNO','CODFUNCGER'], 1)
-                os_finalizadas28 = self.agrupar(m_atual28, ['DT_TURNO','CODFUNCOS'], 1)
-
-                pendencia = m_atual28.loc[m_atual28['POSICAO'] =='P']
-                os_pedentes28 = self.agrupar(pendencia, ['DT_TURNO','CODFUNCGER'], 1)
+                os_geradas28 = self.agrupar(m_atual28, ['DT_TURNO','CODFUNCGER'])
+                os_finalizadas28 = self.agrupar(m_atual28, ['DT_TURNO','CODFUNCOS'])
+                os_pedentes28 = self.agrupar(pendencia, ['DT_TURNO','CODFUNCGER'])
                 print(f"debug: T_28")
             except Exception as e:
                 self.validar_erro(e, "T_28")
@@ -163,13 +129,17 @@ class Abastecimento(auxiliar):
                 return False
             try:
                 m_atual64['CODEMPILHADOR'] = m_atual64['CODEMPILHADOR'].fillna(0)  
-                m_atual64 = self.organizar_df(m_atual64, 'DATAGERACAO', 2)
+                m_atual64['DATAGERACAO'] = pd.to_datetime(m_atual64['DATAGERACAO']).dt.normalize()
+                m_atual64['MES'] = m_atual64['DATAGERACAO'].dt.month
+                m_atual64 = m_atual64[['DATAGERACAO', 'DTLANC', 'NUMBONUS', 'NUMOS', 'CODEMPILHADOR','EMPILHADOR', 'MES']].copy()
+                m_atual64 = m_atual64.drop_duplicates(subset=['NUMOS'])
+
                 m_atual64['DATA'] = pd.to_datetime(m_atual64['DATAGERACAO'].dt.date)
                 m_atual64['HORA'] = m_atual64['DATAGERACAO'].dt.time
                 m_atual64['HORA'] = m_atual64["HORA"].apply(self.corrigir_hr)
                 cond_turno = (
-                    (m_atual64['HORA'] >= "07:00:00")  & (m_atual64['HORA'] <= "18:00:00")
-                    ,(m_atual64['HORA'] >= "18:00:00") | (m_atual64['HORA'] < "07:00:00")
+                    (m_atual64['HORA'] >= self.hr_AM)  & (m_atual64['HORA'] <= self.hr_PM)
+                    ,(m_atual64['HORA'] >= self.hr_PM) | (m_atual64['HORA'] < self.hr_AM)
                 )
                 result_turno = (
                     m_atual64['DATA']
@@ -177,7 +147,9 @@ class Abastecimento(auxiliar):
                 )
                 m_atual64['DT_TURNO'] = np.select(cond_turno, result_turno, default=m_atual64['DATA'])
                 
-                agrupamento = self.agrupar(m_atual64, ['DT_TURNO', 'CODEMPILHADOR'], 2).fillna(0)
+                agrupamento = m_atual64.groupby(['DT_TURNO', 'CODEMPILHADOR']).agg(
+                    OS_RECEB = ("NUMOS", 'nunique')
+                ).reset_index().fillna(0)
                 agrupamento['CODFUNCGER'] = 1
 
                 os_finalizadas64 = agrupamento.loc[agrupamento['CODEMPILHADOR'] != 0]
@@ -197,12 +169,20 @@ class Abastecimento(auxiliar):
                 fim_total = os_finalizadas28.merge(os_finalizadas64, left_on=['DT_TURNO','CODFUNCOS'], right_on= ['DT_TURNO','CODFUNCOS'], how= 'left').drop(columns='CODFUNCGER').fillna(0)
 
                 """Ordem de serviço geradas"""
-                grupo64 = self.agrupar(agrupamento, 'DT_TURNO', 3)
-                grupo28 = self.agrupar(os_geradas28, 'DT_TURNO', 4)
+                grupo64 = agrupamento.groupby('DT_TURNO').agg(
+                    OS_RECEB = ('OS_RECEB', 'sum')
+                ).reset_index()
+                grupo64['DT_TURNO'] = pd.to_datetime(grupo64['DT_TURNO']).dt.normalize()
 
+                grupo28 = os_geradas28.groupby('DT_TURNO').agg(
+                    QTDE_OS = ("QTDE_OS", 'sum'),
+                    OS_50 = ("OS_50", 'sum'),
+                    OS_61 = ("OS_61", 'sum'),
+                    OS_58 = ("OS_58", 'sum')
+                ).reset_index()
+                grupo28['DT_TURNO'] = pd.to_datetime(grupo28['DT_TURNO']).dt.normalize()
+                
                 geral_total = grupo28.merge(grupo64, left_on='DT_TURNO', right_on= 'DT_TURNO', how= 'left').fillna(0)
-
-                bonus = self.agrupar(m_atual64, 'DTLANC', 5) 
             except Exception as e:
                 print(f"debug T_GERAL: {e}")
                 self.validar_erro(e, "T_GERAL")
@@ -214,7 +194,6 @@ class Abastecimento(auxiliar):
             pd_total.to_excel(Power_BI.abst_pd, index= False, sheet_name= "OS_PD")
             fim_total.to_excel(Power_BI.abst_fim, index= False, sheet_name= "OS_FIM")
             geral_total.to_excel(Power_BI.abst_geral, index= False, sheet_name= "OS_GERAL")
-            bonus.to_excel(Power_BI.abst_bonus, index= False, sheet_name= "BONUS")
             print("Laod - finish")
             return True, periodo
         except Exception as e:
