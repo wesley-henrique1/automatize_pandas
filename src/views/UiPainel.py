34411fb7201a36117802c68024712ded651286a9
Wesley Henrique <wesleyhfo123@gmail.com>
2026-05-29 17:18:26 -0300

    feat(painel): inclusão da lógica de inicialização
    Implementada a lógica de inicialização do painel, gerenciando a abertura
    dos arquivos selecionados e preparando o ambiente para a execução dos módulos.

--- src/views/UiPainel.py
+++ src/views/UiPainel.py
@@ -1,7 +1,13 @@
+from ..lib import ValidarErros
 import tkinter as tk
-
+from tkinter import messagebox
+import time
+import subprocess
+import os 
 
 class PainelLog:
+    validador = ValidarErros(fonte="Painel Operação")
+
     def __init__(self, mapa):
         self.background = "#2F4F4F"
         self.frame_color = "#F0FFFF"
@@ -36,10 +42,30 @@ class PainelLog:
         self.executaveis()
         self.Localizar()
         
-        # root.mainloop()
         pass
     def start_UI(self):
         selecionados = [nome for nome, var in self.boolchecks.items() if var.get()]
+        """
+            etapa: abrir os arquivos de saida em ordem sem atropelamento, usar o time para fazer essa pausa
+        """
+        try:
+            if not selecionados:
+                messagebox.showwarning("Aviso", "Selecione ao menos uma opção!")
+                return
+
+            for var in selecionados:
+                caminho = self.path[var]
+                print(f"{var}= {caminho}")
+
+                processo = subprocess.Popen(caminho, shell= True)
+                if processo.poll() is None:
+                    print(f"Sucesso: O programa foi aberto e está rodando (PID: {processo.pid})")
+                else:
+                    print("Aviso: O programa tentou abrir, mas fechou logo em seguida.")
+                time.sleep(1)
+                # os.startfile(caminho)
+        except Exception as e:
+            self.validador.registrar_log(e, "Painel-start_UI")
 
         print(selecionados)
         pass
@@ -62,7 +88,6 @@ class PainelLog:
             self.list_check.append(check)
 
         for chave_interna, texto_exibicao in self.estados.items():
-            print(f"chave: {chave_interna}, item: {texto_exibicao}")
             bloco = tk.Label(
                 self.log,
                 text=f"{chave_interna}: {texto_exibicao}",
@@ -124,7 +149,6 @@ class PainelLog:
             ,highlightbackground= self.borda_color
             ,highlightthickness= 3
         )
-        """Criar o log de estagio indentificar oque deu erro"""
         self.log = tk.Frame(
             janela_principal
             ,bg=self.frame_color
